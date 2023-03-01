723a6cc881e3bb8921312d88f9bf78c50430fa4b
Attila Starkenius <symphtom@hotmail.com>
2023-03-01 08:04:06 +0100

    Attila Starkenius 1.3.2023. Adding EmployeeOverview.razor

---
+++ BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,15 @@
+﻿using BethanysPieShopHRM.App.Models;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanysPieShopHRM.App.Pages
+{
+    public partial class EmployeeOverview
+    {
+        public List<Employee>? Employees { get; set; } = default!;
+
+        protected override void OnInitialized()
+        {
+            Employees = MockDataService.Employees;
+        }
+    }
+}
